d72b3e5430c7a740597fadc323bee5bef66f0d15
Md. Hasibur Rahman <hasibkyau.cse@gmail.com>
2023-06-17 00:08:42 +0600

    optimized code

--- HackerRank/DetectingCycle/DetectingCycle_05.cpp
+++ HackerRank/DetectingCycle/DetectingCycle_05.cpp
@@ -1,102 +1,48 @@
 #include <iostream>
 #include <vector>
-
 using namespace std;
-
+//Declaring varibles
 vector<int> mat[11];
-int N,M,DP[11],cycle[11],tempCycle[11], sum=1e9, tempSum=0;
-
-void initDP(){
-    for(int i=0; i<11; i++){
-        DP[i]=0;
-    }
-}
-
-void initTemp(){
-    tempSum=0;
-    for(int i=0; i<11; i++){
-        tempCycle[i]=0;
-    }
-}
-
-void cmpTempCycle(){
-    tempSum=0;
-    for(int i=1; i<11; i++){
-//       cout << DP[i] << " ";
-       if(1==DP[i]){
-            tempSum+=i;
-       }
-    }
-    if(tempSum<=sum){
-        for(int i=1; i<=11; i++){
-            cycle[i]=DP[i];
-        }
-    }
-//    cout << endl;
-}
-
-void initCycle(){
-    for(int i=0; i<11; i++){
-        cycle[i]=0;
-    }
-}
-
+int N,M,u,v,DP[11],cycle[11], sum=1e9, tempSum=0;
+//Read inputs
 void readCase(){
     scanf("%d%d",&N,&M);
-    for(int i=1; i<=M; i++){
-    int u,v;
-    scanf("%d%d",&u,&v);
-    mat[u].push_back(v);
-    }
+    for(int i=1; i<=M; i++) scanf("%d%d",&u,&v), mat[u].push_back(v);
 }
-
-void printCase(int x){
-    for(int i=x; i<=N; i++){
-        cout << i << ":";
-        for(int j=0; j<mat[i].size(); j++){
-            cout<<mat[i][j]<<" ";
-        }
-        cout << endl;
-    }
+//compare the newly found cycle with previous cycle
+void cmpTempCycle(){
+    tempSum=0;
+    for(int i=1; i<11; i++)if(1==DP[i])tempSum+=i;//calculate the cost
+    if(tempSum<=sum) for(int i=1; i<=11; i++) cycle[i]=DP[i];//if the cost is less than previous one update the previous one
 }
-
+//detect cycles of nodes adjacent edge
 bool detectCycle(int node, int edge){
     bool x;
-    if(node==edge){
-        cmpTempCycle();
-        return true;
-    }
+    if(node==edge){cmpTempCycle(); return true;}
     if(1==DP[edge]) return false;
-    DP[edge]=1;
-
+    DP[edge]=1;//visit node and go forward
+    //if the start node not found
     for(auto i:mat[edge]){
         bool x = detectCycle(node, i);
         if(i!=node)DP[i]=0;
     }
     return x;
 }
-
+//Detect all posible cycles for each node
 void DFS(int node){
-//    cout << "Node: " << node << endl;
-    for(auto edge:mat[node]){
-        bool loop = detectCycle(node, edge);
-    }
-    return;
+    for(auto edge:mat[node]) bool loop = detectCycle(node, edge);
 }
-
+//main function start
 int main(){
     freopen("input.txt","r",stdin);
     readCase();
-//    printCase(1);
-
-
-
-    for(int i=1; i<=N; i++){
-        initDP();
-        DP[i]=1;
+    for(int i=0; i<11; i++) cycle[i]=0;//initialize Cycle
+    for(int i=1; i<=N; i++){//passing each nodes of the graph to detect all possible cylcles
+        for(int j=0; j<11; j++) DP[j]=0;//initialized DP
+        DP[i]=1;//the parent node is always visited
         DFS(i);
     }
-//    cout << endl;
+    //Print the cycle with the min cost
     for(int i=1; i<11; i++) if(cycle[i]==1) printf("%d ",i);
     cout << endl;
     return 0;
